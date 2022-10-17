1b8d6781d9ccb42e87cc6b0b2e2ebfcb6b297599
Bbtee01 <odunukansamuel@gmail.com>
2022-10-17 05:36:46 -0700

    add

--- 1-digit.c
+++ 1-digit.c
@@ -6,9 +6,11 @@
  * @ap: the argument pointer
  * Return: void
  */
+
 void init_params(params_t *params, va_list ap)
 {
 	params->unsign = 0;
+
 	params->plus_flag = 0;
 	params->space_flag = 0;
 	params->hashtag_flag = 0;
